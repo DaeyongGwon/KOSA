8d76122fdeb5d46492d2b1a58697d21395d2a2ed
dae <cys964@gmail.com>
2024-04-30 17:35:16 +0900

    백준 반복문 더하기 문제 풀이

--- 백준/src/Main/Plus3.java
+++ 백준/src/Main/Plus3.java
@@ -3,14 +3,18 @@ package Main;
 import java.util.Scanner;
 
 public class Plus3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(n);
-        for(int i = 0; i < n; i++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            System.out.println(A + B);
-        }
-    }
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();
+
+		for (int i = 0; i < T; i++) {
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			if (A > 0 && B < 10) {
+				System.out.println(A + B);
+			}
+		}
+		scanner.close();
+	}
 }
