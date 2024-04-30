32bc6e67c5d4dddd4d071d39f4f94f332df07ebc
dae <cys964@gmail.com>
2024-04-30 17:36:12 +0900

    접근 지정자 실습
    public static default protected 활용 예제

---
+++ 0430/src/Pack2/C.java
@@ -0,0 +1,10 @@
+package Pack2;
+
+import Pack1.A;
+public class C {
+	public void print() {
+		A a = new A();
+		System.out.println(a.a + " ");
+		System.out.println();
+	}
+}
---
+++ 0430/src/Pack2/D.java
@@ -0,0 +1,10 @@
+package Pack2;
+
+import Pack1.A;
+public class D extends A{
+	public void print() {
+		System.out.println(a + " ");
+		System.out.println(b + " ");
+		System.out.println();
+	}
+}
