4341b365b3d801c9d2f3950be84e0f0f747bd778
dae <cys964@gmail.com>
2024-05-03 17:56:55 +0900

    UserException 예제 코드

---
+++ 0503/src/exception/ExceptionMethod_2.java
@@ -0,0 +1,26 @@
+package exception;
+
+class A1 {
+	void abc() throws NumberFormatException {
+		bcd();
+	}
+	void bcd() throws NumberFormatException{
+		cde();
+	}
+	void cde() throws NumberFormatException {
+		int num = Integer.parseInt("10A");
+	}
+}
+public class ExceptionMethod_2 {
+	public static void main(String[] args) {
+		
+		// 1. 객체 생성 
+		A1 a = new A1();
+		// 2. 메서드 호출 + 예외 처리 
+		try {
+			a.abc();
+		} catch(NumberFormatException e) {
+			e.printStackTrace();
+		}
+	}
+}
